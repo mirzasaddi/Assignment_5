5e7ce25c6021feefb09db805deb884a7f3bcdc97
Mirza Sadi <mirzasaddi@gmail.com>
2025-03-25 05:02:41 -0400

    this is updated server file

--- Web700-app/server.js
+++ Web700-app/server.js
@@ -8,7 +8,6 @@
 *
 *  Online (Vercel) Link: 
 ********************************************************************************/
-
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -18,25 +17,24 @@ const collegeData = require("./modules/collegeData");
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-require('pg'); // explicitly require the "pg" module
-const Sequelize = require('sequelize');
+require('pg'); // Required for Vercel deployment using Sequelize
 
 // Set EJS as view engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", __dirname + "/views");
 
 // Use express-ejs-layouts
 app.use(expressLayouts);
 app.set("layout", "layouts/main");
 
-// Middleware to parse form data from POST
+// Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from /public
-app.use(express.static(path.join(__dirname, "/public")));
+// Serve static files from public
+app.use(express.static(__dirname + "/public"));
 
 // Middleware to set active route
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     let route = req.path.substring(1);
     app.locals.activeRoute = "/" + (
         isNaN(route.split('/')[1]) 
@@ -46,51 +44,44 @@ app.use(function (req, res, next) {
     next();
 });
 
-// EJS custom helpers
+// EJS helper for active nav
 app.locals.navLink = function (url, options) {
     return '<li' +
         ((url == app.locals.activeRoute) ? ' class="nav-item active"' : ' class="nav-item"') +
         '><a class="nav-link" href="' + url + '">' + options.fn(this) + '</a></li>';
 };
 
+// EJS helper to compare values
 app.locals.equal = function (lvalue, rvalue, options) {
     if (arguments.length < 3)
         throw new Error("Helper 'equal' needs 2 parameters");
     return (lvalue != rvalue) ? options.inverse(this) : options.fn(this);
 };
 
-
 // ROUTES
 
-
-// Home Page
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-// About Page
 app.get("/about", (req, res) => {
     res.render("about");
 });
 
-// HTML Demo Page
 app.get("/htmlDemo", (req, res) => {
     res.render("htmlDemo");
 });
 
-// Show Add Student Form
 app.get("/students/add", (req, res) => {
     res.render("addStudent");
 });
 
-// Handle Add Student Form Submission
 app.post("/students/add", (req, res) => {
     collegeData.addStudent(req.body)
         .then(() => res.redirect("/students"))
         .catch(() => res.json({ message: "Unable to add student" }));
 });
 
-// Students (list or filter by course)
 app.get("/students", (req, res) => {
     if (req.query.course) {
         collegeData.getStudentsByCourse(req.query.course)
@@ -103,47 +94,40 @@ app.get("/students", (req, res) => {
     }
 });
 
-// Individual Student Page
 app.get("/student/:studentNum", (req, res) => {
     collegeData.getStudentByNum(req.params.studentNum)
         .then((student) => res.render("student", { student }))
         .catch(() => res.status(404).send("Student Not Found"));
 });
 
-// Update Student
 app.post("/student/update", (req, res) => {
     collegeData.updateStudent(req.body)
         .then(() => res.redirect("/students"))
         .catch(() => res.status(500).send("Unable to update student"));
 });
 
-// TAs (optional if used)
-app.get("/tas", (req, res) => {
-    collegeData.getTAs()
-        .then((tas) => res.json(tas))
-        .catch(() => res.json({ message: "no results" }));
-});
-
-// Courses list
 app.get("/courses", (req, res) => {
     collegeData.getCourses()
         .then((courses) => res.render("courses", { courses }))
         .catch(() => res.render("courses", { message: "no results" }));
 });
 
-// Course Details (read-only view)
 app.get("/course/:id", (req, res) => {
     collegeData.getCourseById(req.params.id)
         .then((course) => res.render("course", { course }))
         .catch(() => res.status(404).send("Course Not Found"));
 });
 
-// 404 - Page Not Found
+app.get("/tas", (req, res) => {
+    collegeData.getTAs()
+        .then((tas) => res.json(tas))
+        .catch(() => res.json({ message: "no results" }));
+});
+
 app.use((req, res) => {
     res.status(404).send("Page Not Found");
 });
 
-// Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server listening on port: " + HTTP_PORT);
     collegeData.initialize();
